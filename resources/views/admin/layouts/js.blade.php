0b007dbb5529ee92713cf6ee9f64475e491d238c
Raynaldi S N <r@student.upi.edu>
2020-09-24 18:03:28 +0700

    templating

---
+++ resources/views/admin/layouts/js.blade.php
@@ -0,0 +1,10 @@
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <script src="{{ asset('admin/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+  <script src="{{ asset('admin/js/ruang-admin.min.js') }}"></script>
+  <script src="{{ asset('admin/vendor/chart.js/Chart.min.js') }}"></script>
+  <script src="{{ asset('admin/js/demo/chart-area-demo.js') }}"></script>  
